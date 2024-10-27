e0b6da570d880b5994422cdd0ad666a8d97f4fd1
Iftala <circlebytes.iftala@gmail.com>
2024-10-27 12:13:47 +0700

    feat: add conditions

---
+++ chapters/loops.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func loops() {
+
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	conferenceName := "Go Conference"
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %v booking application.\nWe have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceName, conferenceTickets, remainingTickets)
+
+	for {
+
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter Your First Name: ")
+		fmt.Scanln(&firstName)
+
+		fmt.Println("Enter Your Last Name: ")
+		fmt.Scanln(&lastName)
+
+		fmt.Println("Enter Your Email: ")
+		fmt.Scanln(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scanln(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		firstNames := []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names %v\n", firstNames)
+	}
+}
